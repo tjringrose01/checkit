2adbe3650a4f4aec50682ee70514ba78790e57b5
Tim Ringrose <73766725+tjringrose01@users.noreply.github.com>
2026-04-22 21:47:24 -0400

    Add authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root "health#show"
   get "/up", to: "health#show"
+
+  resource :session, only: [ :new, :create, :destroy ]
+  resource :password_change, only: [ :edit, :update ]
+
+  root "dashboard#show"
 end
